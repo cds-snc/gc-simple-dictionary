d5a1300906ad3d27af7b67bf92c10b6437383f8b
Wissam Moussa <wissam.moussa@cds-snc.ca>
2020-10-24 23:32:30 -0400

    initialize

---
+++ data.js
@@ -0,0 +1,16 @@
+var data = [
+    { label: 'ATO', value: 'Authority To Operate', explanation: "This is the compliance paperwork that gets filed by software operators within in government. Approvers are often CTOs or CISOs, and they are evaluating risk. However, the paperwork and processes are very burdensome, stem from waterfall days where there were only 1-2 releases a year, and often don't provide the information someone would actually need to measure risk. Streamlining this process is a goal of CDS's. Based on US's NIST 800-53 rev 4."},
+    { label: 'ATS', value: 'Applicant Tracking System', explanation: 'We use Lever at CDS'},
+    { label: 'CBSO', value: 'Cloud-based Service Owner'},
+    { label: 'CCCS', value: 'Canadian Centre for Cyber Security'},
+    { label: 'CSE', value: 'Communications Security Establishment'},
+    { label: 'CSEMP', value: 'Cyber Security Event Management Plan'},
+    { label: 'CSP', value: 'Cloud Service Provider'},
+    { label: 'ESDC', value: 'Employment and Social Development Canada'},
+    { label: 'Float', value: 'Resource Planning & Management Software'},
+    { label: 'GC', value: 'Government of Canada'},
+    { label: 'GCconnex ', value: 'online messaging board'},
+
+    { label: 'CDS', value: 'Canadian Digital Service', explanation: 'yours truly', link: 'https://digital.canada.ca'},
+    { label: 'ATIP', value: 'Access to Information and Privacy', explanation: 'Canadians can request internal records and communications from the government (i.e. ATIP requests). Recent ones have included records of Slack and email chats around COVID Alert.'}
+];
