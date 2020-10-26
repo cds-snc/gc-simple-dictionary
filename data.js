2b01a0bb51406c76e547f52dbd3d544be81143d3
wmoussa-cds <70960477+wmoussa-cds@users.noreply.github.com>
2020-10-26 15:44:53 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,16 +1,33 @@
 var data = [
+    { label: 'ATIP', value: 'Access to Information and Privacy', explanation: 'Canadians can request internal records and communications from the government (i.e. ATIP requests). Recent ones have included records of Slack and email chats around COVID Alert.'},
     { label: 'ATO', value: 'Authority To Operate', explanation: "This is the compliance paperwork that gets filed by software operators within in government. Approvers are often CTOs or CISOs, and they are evaluating risk. However, the paperwork and processes are very burdensome, stem from waterfall days where there were only 1-2 releases a year, and often don't provide the information someone would actually need to measure risk. Streamlining this process is a goal of CDS's. Based on US's NIST 800-53 rev 4."},
     { label: 'ATS', value: 'Applicant Tracking System', explanation: 'We use Lever at CDS'},
     { label: 'CBSO', value: 'Cloud-based Service Owner'},
     { label: 'CCCS', value: 'Canadian Centre for Cyber Security'},
+    { label: 'CDS', value: 'Canadian Digital Service', explanation: 'yours truly', link: 'https://digital.canada.ca'},
     { label: 'CSE', value: 'Communications Security Establishment'},
     { label: 'CSEMP', value: 'Cyber Security Event Management Plan'},
     { label: 'CSP', value: 'Cloud Service Provider'},
     { label: 'ESDC', value: 'Employment and Social Development Canada'},
     { label: 'Float', value: 'Resource Planning & Management Software'},
     { label: 'GC', value: 'Government of Canada'},
-    { label: 'GCconnex ', value: 'online messaging board'},
+    { label: 'GCconnex ', value: 'Online messaging board'},
+    { label: 'GCKey', value: 'Single Sign On attempt by the Office of the CIO within TBS'},
+    { label: 'HC', value: 'Health Canada'},
+    { label: 'Micromission', value: '3-6 months work assignment. Micromissionee usually comes from other GC department ie secondment'},
+    { label: 'MINO', value: "Minister's Office"},
+    { label: 'PF', value: 'Progression Framework', explanation: "Performance management and feedback framework for evaluating performance and communicating expectations to CDS employees. Every community at CDS has one, but all share the same five core competencies.", link:"https://docs.google.com/document/d/1fFXGg_aeE95dDTz3VRBprqNhNwZEeBIY41VTEZjOMGk/edit"},
+    { label: 'PHAC', value: 'Public Health Agency of Canada'},
+    { label: 'PMA', value: 'Performance Management Agreement'},
+    { label: 'PSPC', value: 'Public Service Performance Management'},
+    { label: 'SA&A', value: 'Security assessment and authorization', explanation: "The structured process through which services are assessed and authorized to operate"},
+    { label: 'SC', value: 'Service Canada'},
+    { label: 'SFT', value: 'Speech from the Throne', link:"https://www.canada.ca/en/privy-council/campaigns/speech-throne/info-speech-from-throne.html"},
+    { label: 'SOP', value: 'Standard Operating Procedures'},
+    { label: 'STT', value: 'Show The Things', explanation: "Bi-Weekly All Staff CDS Showcases"},
+    { label: 'TBS', value: 'Treasury Board of Canada'},
+    { label: 'IITB', value: 'Innovation Information and Technology Branch', explanation: 'IT arm of ESDC'}
 
-    { label: 'CDS', value: 'Canadian Digital Service', explanation: 'yours truly', link: 'https://digital.canada.ca'},
-    { label: 'ATIP', value: 'Access to Information and Privacy', explanation: 'Canadians can request internal records and communications from the government (i.e. ATIP requests). Recent ones have included records of Slack and email chats around COVID Alert.'}
+
+
 ];
