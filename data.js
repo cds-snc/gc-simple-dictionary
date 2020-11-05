481f349de54b35be2d8d209871ef2d3df7351180
wmoussa-cds <70960477+wmoussa-cds@users.noreply.github.com>
2020-11-05 09:11:58 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 var data = [
+    { label: 'API', value: 'Application Programming Interface', explnation:"This is a concept in software technology that essentially refers to how multiple applications can interact with and obtain data from one another."},
     { label: 'ATIP', value: 'Access to Information and Privacy', explanation: 'Canadians can request internal records and communications from the government (i.e. ATIP requests). Recent ones have included records of Slack and email chats around COVID Alert.'},
     { label: 'ATO', value: 'Authority To Operate', explanation: "This is the compliance paperwork that gets filed by software operators within in government. Approvers are often CTOs or CISOs, and they are evaluating risk. However, the paperwork and processes are very burdensome, stem from waterfall days where there were only 1-2 releases a year, and often don't provide the information someone would actually need to measure risk. Streamlining this process is a goal of CDS's. Based on US's NIST 800-53 rev 4."},
     { label: 'ATS', value: 'Applicant Tracking System', explanation: 'We use Lever at CDS'},
@@ -10,13 +11,16 @@ var data = [
     { label: 'CSE', value: 'Communications Security Establishment'},
     { label: 'CSEMP', value: 'Cyber Security Event Management Plan'},
     { label: 'CSP', value: 'Cloud Service Provider'},
+    { label: 'ELB', value: 'Elastic Load Balancer', explanation:'an AWS service', link:"https://aws.amazon.com/elasticloadbalancing/"},
     { label: 'ESDC', value: 'Employment and Social Development Canada'},
+    
     { label: 'Float', value: 'Resource Planning & Management Software'},
     { label: 'Form 9200', explanation: "Requisition for Goods and Services and Construction form"},
     { label: 'GC', value: 'Government of Canada'},
     { label: 'GCconnex ', value: 'Online messaging board', explanation:"GCconnex is a professional networking and collaborative workspace for all public servants, allowing them to connect and share information, leveraging the power of networking towards a more effective and efficient public service.",link:"https://gcconnex.gc.ca/login"},
     { label: 'GCKey', value: 'Single Sign On attempt by the Office of the CIO within TBS'},
     { label: 'HC', value: 'Health Canada'},
+    { label: 'k8s', value: 'Kubernestes', explanation:'Kubernetes (K8s) is an open-source system for automating deployment, scaling, and management of containerized applications.'},
     { label: 'Micromission', value: '3-6 months work assignment. Micromissionee usually comes from other GC department ie secondment'},
     { label: 'MINO', value: "Minister's Office"},
     { label: 'PF', value: 'Progression Framework', explanation: "Performance management and feedback framework for evaluating performance and communicating expectations to CDS employees. Every community at CDS has one, but all share the same five core competencies.", link:"https://docs.google.com/document/d/1fFXGg_aeE95dDTz3VRBprqNhNwZEeBIY41VTEZjOMGk/edit"},
