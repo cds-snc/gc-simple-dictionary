b4a19c843803cb044f758779407e68d33a0890ee
wmoussa-cds <70960477+wmoussa-cds@users.noreply.github.com>
2020-11-10 13:18:35 -0500

    Update data.js

--- data.js
+++ data.js
@@ -12,6 +12,7 @@ var data = [
     { label: 'CSP', value: 'Cloud Service Provider'},
     { label: 'ELB', value: 'Elastic Load Balancer', explanation:'an AWS service', link:"https://aws.amazon.com/elasticloadbalancing/"},
     { label: 'ESDC', value: 'Employment and Social Development Canada'},
+    { label: 'FES', value: 'Fall Economic Statement', explanation:"The budget, usually tabled early in the year, is generally preceded in the fall by another major statement, the Fall Economic Statement.", link:"https://www.canada.ca/en/department-finance/services/publications/fall-economic-statement.html"},
     { label: 'Float', value: 'Resource Planning & Management Software'},
     { label: 'Form 9200', explanation: "Requisition for Goods and Services and Construction form"},
     { label: 'GC', value: 'Government of Canada'},
