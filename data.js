61c4fb02d758565c79a66a8baa6be2fa8daca62a
Wissam Moussa <70960477+wmoussa-cds@users.noreply.github.com>
2021-02-25 11:45:49 -0500

    Update data.js

--- data.js
+++ data.js
@@ -5,6 +5,7 @@ var data = [
     { label: 'ATIP', value: 'Access to Information and Privacy', explanation: 'Canadians can request internal records and communications from the government (i.e. ATIP requests). Recent ones have included records of Slack and email chats around COVID Alert.'},
     { label: 'ATO', value: 'Authority To Operate', explanation: "This is the compliance paperwork that gets filed by software operators within in government. Approvers are often CTOs or CISOs, and they are evaluating risk. However, the paperwork and processes are very burdensome, stem from waterfall days where there were only 1-2 releases a year, and often don't provide the information someone would actually need to measure risk. Streamlining this process is a goal of CDS's. Based on US's NIST 800-53 rev 4."},
     { label: 'ATS', value: 'Applicant Tracking System', explanation: 'We use Lever at CDS'},
+    { label: 'BOEW', value: 'Boîte à outils de l’expérience Web',explanation:"Une bibliothèque de code primée pour construire des sites Web accessibles, faciles d'emploi, interopérables, optimisés pour les appareils mobiles et multilingues", link:'https://wet-boew.github.io/wet-boew/index-fr.html'},
     { label: 'CBSA', value: 'Canada Border Services Agency'},
     { label: 'CBSO', value: 'Cloud-based Service Owner'},
     { label: 'CCCS', value: 'Canadian Centre for Cyber Security'},
